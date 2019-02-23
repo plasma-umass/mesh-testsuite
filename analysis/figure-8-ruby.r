068a94c12f103b0744f7cbf8269671ca9090811a
Bobby Powers <bobbypowers@gmail.com>
2019-02-22 22:11:55 -0800

    figure-8-ruby

---
+++ analysis/figure-8-ruby.r
@@ -0,0 +1,54 @@
+library(ggplot2)
+library(grid)
+library(scales)
+
+dat_glibc <- read.delim('results/3-ruby/memory/libc.tsv', header = TRUE, sep = '\t')
+dat_glibc$alloc <- 'glibc'
+dat_glibc$heap <- (dat_glibc$rss + dat_glibc$kernel) / 1024.0 / 1024.0
+dat_glibc$time <- dat_glibc$time / 1000000000.0
+
+dat_jemalloc <- read.delim('results/3-ruby/memory/jemalloc.tsv', header = TRUE, sep = '\t')
+dat_jemalloc$alloc <- 'jemalloc'
+dat_jemalloc$heap <- (dat_jemalloc$rss + dat_jemalloc$kernel) / 1024.0 / 1024.0
+dat_jemalloc$time <- dat_jemalloc$time / 1000000000.0
+
+dat_mesh2n <- read.delim('results/3-ruby/memory/mesh2n.tsv', header = TRUE, sep = '\t')
+dat_mesh2n$alloc <- 'Mesh (no meshing)'
+dat_mesh2n$heap <- (dat_mesh2n$rss + dat_mesh2n$kernel) / 1024.0 / 1024.0
+dat_mesh2n$time <- dat_mesh2n$time / 1000000000.0
+
+dat_mesh0y <- read.delim('results/3-ruby/memory/mesh0y.tsv', header = TRUE, sep = '\t')
+dat_mesh0y$alloc <- 'Mesh (no rand)'
+dat_mesh0y$heap <- (dat_mesh0y$rss + dat_mesh0y$kernel) / 1024.0 / 1024.0
+dat_mesh0y$time <- dat_mesh0y$time / 1000000000.0
+
+dat_mesh2y <- read.delim('results/3-ruby/memory/mesh2y.tsv', header = TRUE, sep = '\t')
+dat_mesh2y$alloc <- 'Mesh'
+dat_mesh2y$heap <- (dat_mesh2y$rss + dat_mesh2y$kernel) / 1024.0 / 1024.0
+dat_mesh2y$time <- dat_mesh2y$time / 1000000000.0
+
+p <- ggplot() +
+    geom_line(data=dat_mesh2n, size=.3, aes(x=time, y=heap, color=alloc, group=2)) +
+    geom_line(data=dat_mesh0y, size=.3, aes(x=time, y=heap, color=alloc, group=3)) +
+    geom_line(data=dat_mesh2y, size=.3, aes(x=time, y=heap, color=alloc, group=5)) +
+    geom_line(data=dat_jemalloc, linetype='dotted', size=.4, aes(x=time, y=heap, color=alloc, group=4)) +
+    scale_y_continuous('RSS (MiB)', expand = c(0, 0), limits = c(0, 300)) +
+    scale_x_continuous('Time Since Program Start (seconds)', expand = c(0, 0), limits = c(0, 3.0)) +
+    theme_bw(10, 'Times') +
+    guides(colour = guide_legend(nrow = 1)) +
+    theme(
+        plot.title = element_text(size=10, face='bold'),
+        strip.background = element_rect(color='dark gray', linetype=0.5),
+        plot.margin = unit(c(.1, .1, 0, 0), 'in'),
+        panel.border = element_rect(colour='gray'),
+        panel.margin = unit(c(0, 0, -0.5, 0), 'in'),
+        legend.position = 'bottom',
+        legend.key = element_rect(color=NA),
+        legend.key.size = unit(0.15, 'in'),
+        legend.margin = unit(0, 'in'),
+        legend.title=element_blank(),
+        axis.title.y = element_text(size=9),
+        axis.text.x = element_text(angle = 0, hjust = 1)
+    )
+
+ggsave(p, filename='results/3-ruby/figure-8-ruby.pdf', width=3.4, height=1.5)
