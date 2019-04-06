bc7b8e66e55ace203e296d2545d17f92cd8b90cb
Bobby Powers <bobbypowers@gmail.com>
2019-04-05 22:44:52 -0700

    redis: add tcmalloc

--- 1-redis/config.py
+++ 1-redis/config.py
@@ -16,5 +16,6 @@ configs = [
     Config('mesh0y',        'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lmesh0y'),
     Config('mesh1y',        'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lmesh1y'),
     Config('mesh2y',        'USE_JEMALLOC=no',          '-Wl,--no-as-needed -lmesh2y'),
+    Config('tcmalloc',      'USE_TCMALLOC_MINIMAL=yes'),
     Config('jemalloc',      'USE_JEMALLOC=yes',         defrag=True),
 ]
